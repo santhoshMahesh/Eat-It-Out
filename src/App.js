792476d5d37339fff41e52bdb9f507c043533e0f
Santhosh Mahesh <santhoshmahesh2001@gmail.com>
2023-07-13 16:02:40 +0530

    Maps integration

--- src/App.js
+++ src/App.js
@@ -1,31 +1,22 @@
 import React, { useState,useContext } from 'react';
-import Header from './components/Layout/Header';
-import Cart from './components/Cart/Cart';
-import CartProvider from './store/CartProvider';
-
-import Meals from './Meals/Meals';
-
-export const UserContext=React.createContext();
+import Maps from './components/Maps/Maps';
+import MealHeader from './SPA/MealHeader';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 
 function App(){
- 
-  const [cartIsShown, setCartIsShown]=useState(false);
 
-  const showCartHandler=()=>{
-    setCartIsShown(true);
-  }
-
-  const hideCartHandler=()=>{
-    setCartIsShown(false);
-  }
   return (
-    <CartProvider>
-      {cartIsShown && <Cart hideCartHandler={hideCartHandler}/>}
-      <Header onShowCart={showCartHandler}/>
-      <main>
-      <Meals/>
-      </main>
-    </CartProvider>
+      <Router>
+        <Switch>
+         <Route exact path='/' component={MealHeader} />
+         <Route path="/Maps" component={Maps}/>
+        </Switch>
+      </Router>
   )
 }
 
---
+++ src/components/Maps/Maps.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { MapContainer } from 'react-leaflet'
+import './Maps.css'
+const Maps = () => {
+  return (
+     <div>
+        Maps
+     </div>
+  )
+}
+
+export default Maps;
